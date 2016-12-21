79a3fece928730c765f8c39a08092b585ac83ad6
Juan Manuel Aguero <juanma.aguero@gmail>
2016-12-21 14:37:50 -0300

    Add description to gallery

--- src/Flowcode/MediaBundle/Form/GalleryType.php
+++ src/Flowcode/MediaBundle/Form/GalleryType.php
@@ -6,24 +6,29 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class GalleryType extends AbstractType {
+class GalleryType extends AbstractType
+{
 
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options) {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
         $builder
-                ->add('name')
-                ->add('enabled')
-                ->add('tags')
-        ;
+            ->add('name')
+            ->add('description', null, [
+                'required' => false,
+            ])
+            ->add('enabled')
+            ->add('tags');
     }
 
     /**
      * @param OptionsResolverInterface $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
         $resolver->setDefaults(array(
             'data_class' => 'Amulen\MediaBundle\Entity\Gallery'
         ));
@@ -32,7 +37,8 @@ class GalleryType extends AbstractType {
     /**
      * @return string
      */
-    public function getName() {
+    public function getName()
+    {
         return 'flowcode_mediabundle_gallery';
     }
 
